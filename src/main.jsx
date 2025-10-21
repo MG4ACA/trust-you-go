83d24f97b4833cf923f064fa8f9a2ae59a2eb2e8
MG4ACA <mg4.aca@gmail.com>
2025-10-22 00:40:13 +0530

    mock server created

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
-import { LanguageProvider } from "./contexts/LanguageContext.jsx";
-import "./index.css";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import { LanguageProvider } from './contexts/LanguageContext.jsx';
 
-createRoot(document.getElementById("root")).render(
+// PrimeReact styles (for admin portal only)
+import 'primeicons/primeicons.css';
+import 'primereact/resources/primereact.min.css';
+import 'primereact/resources/themes/lara-light-blue/theme.css';
+
+import './index.css';
+
+createRoot(document.getElementById('root')).render(
   <StrictMode>
     <LanguageProvider>
       <App />
